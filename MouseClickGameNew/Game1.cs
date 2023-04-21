c1741a2412cbc4ea3f889391be8fdb0121eea2f0
GoddlyGut <a.reitman@icloud.com>
2023-04-21 13:03:04 -0400

    created grid placing

--- MouseClickGameNew/Game1.cs
+++ MouseClickGameNew/Game1.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
 
 namespace MouseClickGameNew
 {
@@ -9,17 +13,37 @@ namespace MouseClickGameNew
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+
+        //GAME BOARD
+        List<int> posToChoose = new List<int>();
+        List<int> XposUsed = new List<int>();
+        List<int> YposUsed = new List<int>();
+        Dictionary<int, int> gridItemsPositions = new Dictionary<int, int>();
+
+
         //GAME STATES
         bool isGameRunning = true;
         int currentLevel = 0;
+        bool isTimeUp = false;
+
 
         //GRID HANDLING
-        int numberOfGridSpaces = 400;
+        int numberOfGridSpaces = 20;
         bool gridCreated = false;
+        int gridWidth = 20;
+        int gridHeight = 20;
+        int cellSize = 30;
+        int gridSpacing = 0;
+
+        //VALUES
+        int numberOfItemsInLevel = 0;
+        int numberOfItemsInLevelMax = 60;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
+            _graphics.PreferredBackBufferWidth = 600; //
+            _graphics.PreferredBackBufferHeight = 600; //
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -27,6 +51,8 @@ namespace MouseClickGameNew
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
+
+            CreatePositionsToRandomlyPick();
 
             base.Initialize();
         }
@@ -43,6 +69,11 @@ namespace MouseClickGameNew
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            if (!gridCreated)
+            {
+                CreateItemsOnGrid();
+            }
+
             // TODO: Add your update logic here
 
             base.Update(gameTime);
@@ -52,9 +83,64 @@ namespace MouseClickGameNew
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
+            _spriteBatch.Begin();
+
+            DrawGridItemsOnScreen();
+
+            _spriteBatch.End();
+
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
+        }
+
+        private void CreatePositionsToRandomlyPick()
+        {
+            for (int i = 0; i < 400; i++)
+            {
+                posToChoose.Add(i);
+            }
+        }
+
+        private void CreateItemsOnGrid()
+        {
+            while (numberOfItemsInLevel < numberOfItemsInLevelMax)
+            {
+                Random random = new Random();
+
+                bool posFound = false;
+
+                while (!posFound)
+                {
+                    int XpositionToCheck = random.Next(posToChoose.Count);
+                    int YpositionToCheck = random.Next(posToChoose.Count);
+                    if (!gridItemsPositions.ContainsKey(XpositionToCheck) && !gridItemsPositions.ContainsValue(YpositionToCheck))
+                    {
+                        posFound = true;
+                        Debug.WriteLine(posToChoose[XpositionToCheck] + ", " + posToChoose[YpositionToCheck]);
+                        gridItemsPositions.Add(posToChoose[XpositionToCheck], posToChoose[YpositionToCheck]);
+                        numberOfItemsInLevel++;
+                    }
+
+                }
+            }
+        }
+
+        private void DrawGridItemsOnScreen()
+        {
+            foreach (KeyValuePair<int, int> pair in gridItemsPositions)
+            {
+                Texture2D texture = Content.Load<Texture2D>("PowerStar");
+
+                int x = (pair.Key % gridWidth) * (cellSize + gridSpacing);
+                int y = (pair.Key / gridWidth) * (cellSize + gridSpacing);
+                Rectangle rect = new Rectangle(x, y, cellSize, cellSize);
+
+
+                _spriteBatch.Draw(texture, rect, Color.White);
+            }
+
+            
         }
     }
 }
